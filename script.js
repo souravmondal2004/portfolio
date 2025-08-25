e3bde42b31913e91510fca868e97535174e18783
souravmondal2004 <souravmondal09864@gmail.com>
2025-08-25 19:56:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings:["Frontend Devoloper","programmer","Web Developer","AI Devoloper"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
